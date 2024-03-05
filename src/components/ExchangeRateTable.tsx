1f0267ea35f266a0b6edd92483931d37c19a8e8a
ZavorotniyDmytro <121565594+ZavorotniyDmytro@users.noreply.github.com>
2024-03-06 01:37:34 +0200

    Update ExchangeRateTable.tsx

--- src/components/ExchangeRateTable.tsx
+++ src/components/ExchangeRateTable.tsx
@@ -8,14 +8,14 @@ export default function ExchangeRateTable({rate}: IExchangeRateTableProp){
     return(
         <table style={{width: "500px"}}>
             <thead>
-                <tr>
+                <tr style={{backgroundColor: "#86989B"}}>
                     <th>Валюта</th>
                     <th>Ціна</th>
                     <th>Код</th>
                 </tr>
             </thead>
             <tbody>
-                {rate.map((r, index)=>(<tr  key={r.r030}>
+                {rate.map((r, index)=>(<tr style={{backgroundColor: index % 2 ? "#86989B" : "#ACBAAA"}} key={r.r030}>
                     <th>{r.txt}</th>
                     <th>{r.rate}</th>
                     <th>{r.cc}</th>
